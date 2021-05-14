ffb0ead5ef9b7d696b33ab0d050069e81ed77ebe
sudhir33 <32663121+sudhir33@users.noreply.github.com>
2021-05-14 09:07:31 +0530

    Update file3.c

--- file3.c
+++ file3.c
@@ -1,35 +1,39 @@
-#include<stdio.h>
-int main()
-{
-	FILE *ptr;
-	char ch,file_name[100];
-	scanf("%[^\n]s",&file_name);
-	ptr=fopen(file_name,"r");
-	if(ptr)
-	{
-		ch=fgetc(ptr);
-		while(ch!=EOF)
-		{
-			printf("%c",ch);
-			ch=fgetc(ptr);
-		}
-	}
-	else
-	{
-		printf("File not found");
-	}
-	fclose(ptr);
-	return 0;
-}
-/*
-file pointer
-FILE *ptr;
-open: fopen()
-syn: fptr=fopen("file_name","mode")
-mode-->  read write append
-          r    w     a
-          
-fgetc()
-EOF-->end of file
+num=int(input())#41232
+mi=num%10# 2
+ma=num%10# 2
+while num:
+    r=num%10
+    num=num//10
+    if r>ma:#3>2
+        ma=r
+    if r<mi:
+        mi=r
+        
+print(mi,ma)
+        
 
-*/
+
+"""
+414112
+
+3 2
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
